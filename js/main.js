fa68b456beed8b55c11177f3d9fb9bc978b3911c
situ0020 <situ0020@algonquinlive.com>
2014-10-02 10:59:10 -0400

    Fixed error

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var sales__1 = $('.sales__1');
-var sales__details__1 = $('.sale__details__1');
+var sales__details__1 = $('.sales__details__1');
 
 var buttonClickHandler = function () {
 	var currentState = sales__details__1.attr('data-state');
@@ -14,7 +14,7 @@ var buttonClickHandler = function () {
 sales__1.on('click', buttonClickHandler);
 
 var sales__2 = $('.sales__2');
-var sales__details__2 = $('.sale__details__2');
+var sales__details__2 = $('.sales__details__2');
 
 var buttonClickHandler = function () {
 	var currentState = sales__details__2.attr('data-state');
