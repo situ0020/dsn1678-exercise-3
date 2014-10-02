d836a4aa04614b621fb3811d3db636bfa4b647b5
situ0020 <situ0020@algonquinlive.com>
2014-10-02 10:50:45 -0400

    Added extra classes for js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
-var sales = $('.sales');
-var sales__details = $('.sale__details');
+var sales__1 = $('.sales__1');
+var sales__details__1 = $('.sale__details__1');
 
 var buttonClickHandler = function () {
-	var currentState = sales__details.attr('data-state');
+	var currentState = sales__details__1.attr('data-state');
 	
 	if (currentState =='active') {
-	sales__details.attr('data-state', 'inactive');
+	sales__details__1.attr('data-state', 'inactive');
 	} else {
-	sales__details.attr('data-state', 'active');
+	sales__details__1.attr('data-state', 'active');
 	}
 };
 
-sales.on('click', buttonClickHandler);
+sales__1.on('click', buttonClickHandler);
 
+var sales__2 = $('.sales__2');
+var sales__details__2 = $('.sale__details__2');
+
+var buttonClickHandler = function () {
+	var currentState = sales__details__2.attr('data-state');
+	
+	if (currentState =='active') {
+	sales__details__2.attr('data-state', 'inactive');
+	} else {
+	sales__details__2.attr('data-state', 'active');
+	}
+};
+
+sales__2.on('click', buttonClickHandler);
 
